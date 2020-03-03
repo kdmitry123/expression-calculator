31feb6d74ede5f09250f28e9bce294a929cd8054
Dzmitry K <dzmitry.karachun@mail.ru>
2020-03-03 13:04:14 +0300

    feat: add sollution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let exprArr = expr.replace(/\s/g, "").split("");
+    console.log(arguments);
+    Errors(exprArr);
+
+    function Errors(exprArr) {
+        let a = 0;
+        for (let i = 0; i < exprArr.length; i++) {
+            if (exprArr[i] === "(") {
+                a++;
+            }
+            if (exprArr[i] === ")") {
+                a--;
+            }
+        }
+        if (a !== 0) {
+            throw new Error("ExpressionError: Brackets must be paired.");
+        }
+
+        for (let i = 0; i < exprArr.length; i++) {
+            if (exprArr[i] === "/") {
+                if (exprArr[i + 1] === "0")
+                    throw new Error("TypeError: Division by zero.");
+            }
+        }
+    }
+
+    total = new Function("return " + expr.replace(/\s/g, ""));
+    return  total();
 }
 
 module.exports = {
